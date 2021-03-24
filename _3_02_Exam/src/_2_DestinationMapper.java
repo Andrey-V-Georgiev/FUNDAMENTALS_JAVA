f7ad7a587666c38a741451cece5ecfc0a69f1ec2
Andrey-V-Georgiev <andrey.v.georgiev@gmail.com>
2021-03-24 19:22:34 +0200

    exam 2 exercise 2(100)

--- _3_02_Exam/src/_2_DestinationMapper.java
+++ _3_02_Exam/src/_2_DestinationMapper.java
@@ -1,6 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class _2_DestinationMapper {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("([=|\\/]){1}([A-Z][a-zA-Z]{2,})\\1");
+        Matcher matcher = pattern.matcher(input);
+        List<String> validLocations = new ArrayList<>();
+
+        /* Obtain the valid locations */
+        while (matcher.find()) {
+            String location = matcher.group(2);
+            validLocations.add(location);
+
+        }
+
+        /* Calculate travel points */
+        int travelPoints = validLocations.stream().mapToInt(String::length).sum();
+
+        /* Print output */
+        System.out.printf("Destinations: %s\n", String.join(", ", validLocations));
+        System.out.printf("Travel Points: %d\n", travelPoints);
     }
 }
